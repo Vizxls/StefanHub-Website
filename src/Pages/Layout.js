f9674d2f97b5265f89237b3aef131b60638731b0
Vizxls <89232729+Vizxls@users.noreply.github.com>
2025-10-24 17:50:54 +1300

    Update Layout.js

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { createPageUrl } from "@/utils";
+import { createPageUrl } from "../utils";
 import { Home, Gamepad2, Key, Github } from "lucide-react";
 
 export default function Layout({ children, currentPageName }) {
